b1ccc82df90eff30fca71c7f6e230855a0773f98
RohanSahana <rohansahana3000@gmail.com>
2022-05-17 13:51:41 +0530

    deploy8

--- djangoMLDeployment/settings.py
+++ djangoMLDeployment/settings.py
@@ -133,7 +133,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [STATIC_DIR]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
